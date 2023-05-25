0d3e5e18e1c77d2f556795db7ee968741e0d8871
Άξονας <128817936+axe-fr@users.noreply.github.com>
2023-05-25 20:29:49 +0200

    Update commands.js

--- handlers/commands.js
+++ handlers/commands.js
@@ -1,5 +1,4 @@
-import { readdirSync } from "fs";
-
+const { readdirSync } = require("fs")
 module.exports = (client) => {
 	readdirSync('./commands/').forEach(dir => {
 		const commands = readdirSync(`./commands/${dir}`).filter(file => file.endsWith('.js'));
