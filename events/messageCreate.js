7ac76453d91430f76dc26683973147df00633783
Άξονας <128817936+axe-fr@users.noreply.github.com>
2023-06-10 00:20:50 +0200

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -7,7 +7,7 @@ run: async (client, message) => {
     if (message.author.id !== message.client.user.id) return;
     if (!message.content.startsWith(config.prefix)) return;
     const args = message.content.slice(config.prefix.length).trim().split(" ")
-    args.shift()
+    const cmd = args.shift().toLowerCase();
     let command = client.commands.get(cmd)
     if (!command) command = client.commands.get(client.aliases.get(cmd));
     if (command) {
@@ -15,3 +15,5 @@ run: async (client, message) => {
        }
     }
 }
+
+
